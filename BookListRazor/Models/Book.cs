90b823c0064b500770494aacd80e8a0630271562
Blast06 <lacalculadora06@hotmail.com>
2019-04-15 20:10:18 -0400

    Add project files.

---
+++ BookListRazor/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Titulo del libro")]
+        public string Name { get; set; }
+        [Display(Name = "Codigo ISBN")]
+        public string ISBN { get; set; }
+        [Display(Name = "Autor")]
+        public string Author { get; set; }
+    }
+}
---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public IEnumerable<Book> Books { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _dbContext = db;
+
+        }
+        public async Task OnGet()
+        {
+            Books = await _dbContext.Books.ToListAsync();
+
+        }
+
+        //para eliminar un libro
+        public async Task<IActionResult> OnPost(int id)
+        {
+            var book = await _dbContext.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+
+            }
+
+            _dbContext.Books.Remove(book);
+            await _dbContext.SaveChangesAsync();
+            Message = "Libro eliminado exitosamente";
+            return RedirectToPage("Index");
+        }
+    }
+}
